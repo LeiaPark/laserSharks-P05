2d3cd81192d963a26c2b6b45a03468e721b3f4ef
Hilary Zen <hzen00@stuy.edu>
2020-06-08 01:08:36 -0400

    Added login and register

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask import session
 from flask import redirect
 from flask import flash
 from flask import url_for
-from utl import api #db_functions, create_db
+from utl import api, db_functions #create_db
 import sqlite3  # enable control of an sqlite database
 import os
 import json
@@ -15,6 +15,53 @@ import random
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+@app.route('/')
+def index():
+    # load the template with the user's session info
+    if 'user' in session:
+        return redirect(url_for('favorites'))
+    else:
+        return render_template('homepage.html')
+
+@app.route('/login')
+def login():
+    if 'user' in session:
+        return redirect(url_for('favorites'))
+    elif request.args:
+        if db_functions.checkfor_credentials(request.args.get('username'), request.args.get('password')):
+            session['user'] = request.args.get('username')
+            return redirect(url_for('favorites'))
+        else:
+            flash('Invalid Credentials')
+            return redirect(url_for('login'))
+    else:
+        return render_template('login.html')
+
+@app.route('/register')
+def register():
+    if 'user' in session:
+        return redirect(url_for('favorites'))
+    elif request.args:
+        if db_functions.checkfor_username(request.args.get('username')):
+            flash('Account with that username already exists')
+            return redirect(url_for('register'))
+        else:
+            db_functions.create_user(request.args.get('username'), request.args.get('password'))
+            flash('Account Created')
+            return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/favorites')
+def favorites():
+    return render_template('favorites.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    flash("Logged Out Succesfully")
+    return redirect(url_for("index"))
+
 if __name__ == "__main__":
     app.debug = True
+    db_functions.create()
     app.run()
---
+++ app/utl/db_functions.py
@@ -0,0 +1,68 @@
+import sqlite3  # enable control of an sqlite database
+DB_FILE = "pokedex.db"
+
+def create():
+    db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+    c = db.cursor()  # facilitate db ops
+
+
+    # < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+
+    #==========================================================
+
+    # test SQL stmt in sqlite3 shell, save as string
+    create_table_users = "CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT);"
+    insert_user = "INSERT OR IGNORE INTO users(user_id, username, password) VALUES( 1, \"admin\", \"stuy\");"
+    create_table_favorites = "CREATE TABLE IF NOT EXISTS favorites(user_id INTEGER PRIMARY KEY AUTOINCREMENT, list TEXT);"
+    insert_favorites = "INSERT OR IGNORE INTO favorites(user_id, list) VALUES( 1, \"1\");"
+
+    c.execute(create_table_users)
+    c.execute(insert_user)
+    c.execute(create_table_favorites)
+    c.execute(insert_favorites)
+
+    db.commit()  # save changes
+    db.close()  # close database
+
+def checkfor_credentials(username, password):
+    db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+    c = db.cursor()  # facilitate db ops
+
+    query = "SELECT username, password FROM users WHERE users.username = \"%s\" AND users.password = \"%s\";" % (username, password)
+    response = list(c.execute(query))
+    db.commit()  # save changes
+    db.close()  # close database
+
+    return response # returns null if credentials are wrong, and the correct info if correct
+
+def checkfor_username(username):
+    db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+    c = db.cursor()  # facilitate db ops
+
+    query = "SELECT username FROM users WHERE username == \"%s\";" % (username)
+    response = list(c.execute(query))
+    db.commit()  # save changes
+    db.close()  # close database
+
+    return response
+
+def create_user(username, password):
+    db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+    c = db.cursor()  # facilitate db ops
+
+    query = "INSERT INTO users(username, password) VALUES(\"%s\", \"%s\");" % (username, password)
+    response = list(c.execute(query))
+    query = "INSERT INTO favorites(list) VALUES(\"\");"
+    response = list(c.execute(query))
+    db.commit()  # save changes
+    db.close()  # close database
+
+def get_user_id(username):
+    db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+    c = db.cursor()  # facilitate db ops
+
+    query = "SELECT user_id FROM users WHERE username == \"%s\";" % (username)
+    response = list(c.execute(query))
+    db.commit()  # save changes
+    db.close()  # close database
+    return response
